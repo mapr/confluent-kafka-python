e902687f63bc3061fae945ad03320d2e7412a1ad
oskobelskyi <oskobelskyi@maprtech.com>
2017-02-16 14:40:27 +0200

    Make producer test_basic_api work with Streams

--- tests/test_Producer.py
+++ tests/test_Producer.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 from confluent_kafka import Producer, KafkaError, KafkaException
+import pytest
+import subprocess
+
+@pytest.fixture(autouse=True)
+def resource_setup(request):
+    subprocess.call(['bash','-c', "maprcli stream topic create -path /stream -topic topic1"])
+    print("resource_setup")
+    def resource_teardown():
+        subprocess.check_call(['bash','-c', "maprcli stream topic delete -path /stream -topic topic1"])
+        print("resource_teardown")
+    request.addfinalizer(resource_teardown)
 
 
 def test_basic_api():
-    """ Basic API tests, these wont really do anything since there is no
-        broker configured. """
-
     try:
         p = Producer()
     except TypeError as e:
@@ -18,17 +26,20 @@ def test_basic_api():
 
     p = Producer({'socket.timeout.ms':10,
                   'error_cb': error_cb,
-                  'default.topic.config': {'message.timeout.ms': 10}})
+                  'default.topic.config': {'message.timeout.ms': 10, 'auto.offset.reset': 'earliest'}})
 
-    p.produce('mytopic')
-    p.produce('mytopic', value='somedata', key='a key')
+    p.produce('/stream:topic1')
+    p.produce('/stream:topic1', value='somedata', key='a key')
+    try:
+        p.produce(None)
+    except TypeError as e:
+        assert str(e) == "argument 1 must be string, not None"
+
 
     def on_delivery(err,msg):
         print('delivery', str)
-        # Since there is no broker, produced messages should time out.
-        assert err.code() == KafkaError._MSG_TIMED_OUT
 
-    p.produce(topic='another_topic', value='testing', partition=9,
+    p.produce(topic='/stream:topic1', value='testing', partition=9,
               callback=on_delivery)
 
     p.poll(0.001)
